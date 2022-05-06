4fff001270c404cd35f5a40e9ab8666cbaf7ee86
dolaver <77640476+dolaver@users.noreply.github.com>
2022-05-06 11:26:10 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,28 +1,18 @@
-import setuptools
+from setuptools import setup
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-setuptools.setup(
-    name="calsagos",
-    version="1.0",
-    description="CALSAGOS: Clustering ALgorithmS Applied to Galaxies in Overdense Systems",
-    license="MIT",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    author="Daniela Olave-Rojas & Pierluigi Cerulo",
-    author_email="daniela.olave@utalca.cl - pcerulo@inf.udec.cl",
-    url = 'https://github.com/dolaver/calsagos',
-    download_url = 'https://github.com/dolaver/calsagos/1.0',
-    packages=['calsagos'],  #same as name
-    keywords = ['astronomy', 'utilities', 'substructures'],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    package_dir={"": ""},
-    packages=setuptools.find_packages(where=""),
-    python_requires=">=3.6",
-)
-
+setup(name='calsagos',
+      version='0.1',
+      description='CALSAGOS: Clustering ALgorithmS Applied to Galaxies in Overdense Systems',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      author='Daniela Olave-Rojas & Pierluigi Cerulo',
+      author_email='daniela.olave@utalca.cl - pcerulo@inf.udec.cl',
+      url = "https://github.com/dolaver/calsagos",
+      packages=['calsagos'],  #same as name
+      scripts = ['clumberi.py', 'cluster_kinematics.py',
+                 'ds_test.py', 'isomer.py', 'lagasu.py',
+                 'redshift_boundaries.py', 'utils.py']
+      )
